e6c3a349483f63e87d8ada9e8028e51741e62995
Legion Script <58012792+legionscript@users.noreply.github.com>
2023-04-14 21:04:47 -0400

    Update settings.py

--- blogtutorial/blogtutorial/settings.py
+++ blogtutorial/blogtutorial/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)@6_684gu73to1zy)@bt^!ya(x9n%zk#7r-)n53wsbv2i2*xkv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
